ecfe88c773502af887eccef4b470ed94747005dc
Adrien Dorsaz <adrien@adorsaz.ch>
2016-08-12 16:01:11 +0200

    agreement: update to latest let's encrypt agreement revision

--- acme_dns_tiny.py
+++ acme_dns_tiny.py
@@ -103,7 +103,7 @@ def get_crt(config, log=LOGGER):
     log.info("Registering account...")
     code, result, headers = _send_signed_request(config["acmednstiny"]["CAUrl"] + "/acme/new-reg", {
         "resource": "new-reg",
-        "agreement": "https://letsencrypt.org/documents/LE-SA-v1.0.1-July-27-2015.pdf",
+        "agreement": "https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf",
     })
     if code == 201:
         log.info("Registered!")
